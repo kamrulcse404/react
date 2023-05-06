5dc94991da088373776c3cf65a6f886444f5bbfc
kamrulcse404 <kamrul.h01960@gmail.com>
2023-05-06 17:18:25 +0600

    api crud

--- posts/src/component/posts.component.jsx
+++ posts/src/component/posts.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 
 class Posts extends Component {
   state = {
@@ -8,52 +9,103 @@ class Posts extends Component {
 
   async componentDidMount() {
     try {
-      const { data: posts } = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      const { data: posts } = await axios.get(apiUrl);
       this.setState({ posts });
     } catch (error) {
       console.log(error);
     }
-
-    // promise
-    //   .then(response => {
-    //     // console.log(response);
-    //     this.setState({ posts: response.data });
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   })
   }
+
+  handleCreate = async () => {
+    try {
+      const post = { title: "Pondit", body: "I like React JS" };
+
+      const { data } = await axios.post(apiUrl, post);
+
+      const posts = [data, ...this.state.posts];
+      this.setState({ posts });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleUpdate = async (postId) => {
+    try {
+      const post = { title: "Updated the Title" };
+
+      const { data } = await axios.put(`${apiUrl}/${postId}`, post);
+
+      const posts = [...this.state.posts];
+      posts.forEach((post) => {
+        if (post.id === postId) {
+          post.title = data.title;
+        }
+      });
+      this.setState({ posts });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleDelete = async (postId) => {
+    try {
+      await axios.delete(`${apiUrl}/${postId}`);
+
+      const posts = [...this.state.posts];
+      posts.filter((post) => {
+        if (post.id !== postId) return true;
+      });
+      this.setState({ posts });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   render() {
     return (
-      <table class="table container">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Title</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.posts.map((post) => {
-            return (
+      <>
+        <div className="container mt-4">
+          <button className="btn btn-primary" onClick={this.handleCreate}>
+            Add Post
+          </button>
+          <table className="table">
+            <thead>
               <tr>
-                <th scope="row">{post.id}</th>
-                <td>{post.title}</td>
-                <td>
-                  <button className="btn btn-primary">Update</button>
-                </td>
-                <td>
-                  <button className="btn btn-danger">Delete</button>
-                </td>
+                <th scope="col">ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {this.state.posts.map((post) => {
+                return (
+                  <tr key={post.id}>
+                    <th scope="row">{post.id}</th>
+                    <td>{post.title}</td>
+                    <td>
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => this.handleUpdate(post.id)}
+                      >
+                        Update
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => this > handleDelete(post.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </>
     );
   }
 }
