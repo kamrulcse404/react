40e9bebbb84fc4d49a534121148378a64e955ee1
kamrulcse404 <kamrul.h01960@gmail.com>
2023-05-06 12:22:42 +0600

    api call using axios

---
+++ posts/src/component/posts.component.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Posts extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    const promise = axios.get("https://jsonplaceholder.typicode.com/posts");
+
+    promise
+      .then(response => {
+        // console.log(response);
+        this.setState({ posts: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  render() {
+    return (
+      <table class="table container">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          this.state.posts.map(post => {
+            return (
+              <tr>
+                <th scope="row">{post.id}</th>
+                <td>{post.title}</td>
+                <td>
+                  <button className="btn btn-primary">Update</button>
+                </td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            );
+          })
+        }
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Posts;
+
+
